b235217404784c39bdd25a0e5bad70804609efae
passosleo <leonardopassos98@gmail.com>
2025-01-23 15:16:20 -0300

    ajustes finais

--- sql-injection/app.js
+++ sql-injection/app.js
@@ -2,10 +2,10 @@ const db = require("./db");
 
 const app = require("fastify")();
 
-app.get("/orders/:id", async (req, _) => {
+app.get("/users/:id", async (req, _) => {
   const { id } = req.params;
 
-  const query = `SELECT * FROM tb_orders WHERE id = ${id}`;
+  const query = `SELECT * FROM users WHERE id = ${id}`;
   console.log("app.get ~ query", query);
 
   const res = await db.query(query);
